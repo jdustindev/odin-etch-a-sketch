e149ff2b3aaae72468d1dd4c25df8091323f6f33
jdustindev <jdustindev@gmail.com>
2022-12-05 18:50:21 -0500

    Add cell color chaning on mouseover

--- etchasketch.js
+++ etchasketch.js
@@ -6,11 +6,19 @@ function generateGrid(size) {
     for (let j=0; j < size; j++) {
       const cell = document.createElement('div');
       cell.classList.add('gridCell');
+
+      cell.addEventListener('mouseover', () => {
+        colorCell(cell);
+      })
+
       row.appendChild(cell);
     }
     gridContainer.appendChild(row);
-
   }
+}
+
+function colorCell(cell) {
+  cell.style.backgroundColor = "#666666";
 }
 
 generateGrid(16);
